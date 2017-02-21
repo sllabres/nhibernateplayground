de36e952d66e1c27449b7dce578b8799cc10f354
Sebastian Llabres <sllabres@gmail.com>
2017-02-21 20:56:18 +0000

    add initial solution, project and packages

---
+++ Nhibernate.One/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Nhibernate.One
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(true, Is.EqualTo(true));
+        }
+    }
+}
